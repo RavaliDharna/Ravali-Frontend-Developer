a26ec0857e4d86841d9139916c714b964d5b9ecf
RavaliDharna <writetoravali@gmail.com>
2022-11-01 17:01:32 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './Home/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+    <Homepage />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Homepage.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./Homepage.css";
+import Navbar from "../Navbar/Navbar";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="first-section">
+        <Navbar />
+        <div className="reWatch">
+          <h4 className="animate">Recent Launch</h4>
+          <h2 className="missionHeader">Starlink Mission</h2>
+          <div className="btn">
+            <a class="reWatchBtn" tabindex="0" href="/launches/sl4-31/">
+              <span class="text">REWATCH</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="second-section">
+          <div className="completedMission">
+            <h4 className="animate">Completed Mission</h4>
+            <h2 className="missionHeader">Crew-4 Returns to Earth</h2>
+            <div className="btn">
+              <a class="reWatchBtn" tabindex="0" href="/launches/sl4-31/">
+                <span class="text">REWATCH</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="Third-section">
+        <div className="">
+            <h4 className="animate">Completed Mission</h4>
+            <h2 className="missionHeader">Crew-4 Returns to Earth</h2>
+            <div className="btn">
+              <a class="reWatchBtn" tabindex="0" href="/launches/sl4-31/">
+                <span class="text">REWATCH</span>
+              </a>
+            </div>
+          </div>
+          <h6>hjhkjhj</h6>
+        </div>
+      </div>
+      <footer className="footer">
+        <div className="footer-content">
+          <div>
+            <ul>
+              <li>
+                <a style={{"color": "#979797"}} href="//#">SPACEX @2022</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="//#">TWITTER</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="//#">YOUTUBE</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="//#">INSTAGRAM</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="//#">FLICKR</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="//#">LINKEDIN</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="/#">PRIVACY POLICY</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="/#">SUPPLIERS</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Homepage;
